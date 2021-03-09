67b2333e6a5516d33849559b9e7d8a078d290ee1
Tomasz Kurcz <uint@lavabit.com>
2021-03-09 14:29:05 +0100

    Add a test for regular structs

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,11 +1,24 @@
 use serbia::serbia;
-
-#[serbia]
-fn bar() -> u32 {
-    2 + 2
-}
+use serde::{Serialize, Deserialize};
 
 #[test]
-fn it_works() {
-    assert_eq!(4, bar());
+fn regular_struct() {
+    #[serbia]
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    struct S {
+        arr_a: [u8; 300],
+        arr_b: [u8; 42],
+        arr_small: [u8; 8],
+    }
+
+    let original = S {
+        arr_a: [0; 300],
+        arr_b: [0; 42],
+        arr_small: [0; 8],
+    };
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
 }
