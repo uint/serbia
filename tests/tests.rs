e3df37c4ca839efaea396312160b00514ac74dda
Tomasz Kurcz <uint@lavabit.com>
2021-03-09 22:26:47 +0100

    Add a test for tuple structs. Hey, they work too, apparently!

--- tests/tests.rs
+++ tests/tests.rs
@@ -22,3 +22,25 @@ fn regular_struct_roundtrip() {
 
     assert_eq!(original, deserialized);
 }
+
+#[test]
+fn tuple_struct_roundtrip() {
+    #[serbia]
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    struct S (
+        [u8; 300],
+        [u8; 42],
+        [u8; 8],
+    );
+
+    let original = S (
+        [0; 300],
+        [0; 42],
+        [0; 8],
+    );
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
+}
