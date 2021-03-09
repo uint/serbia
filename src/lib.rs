fb63c36f18d877ea1ba5e553b5e933507d5ae4cc
Tomasz Kurcz <uint@lavabit.com>
2021-03-09 22:23:07 +0100

    Implement deserialization for structs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{Expr, ExprLit, Field, ItemStruct, Lit, Meta, NestedMeta, Type, parse_macro_input, parse_quote};
+use syn::{
+    parse_macro_input, parse_quote, Expr, ExprLit, Field, ItemStruct, Lit, Meta, NestedMeta, Type,
+};
 
 #[proc_macro_attribute]
 pub fn serbia(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -53,9 +55,14 @@ pub fn serbia(_attr: TokenStream, item: TokenStream) -> TokenStream {
     }
 
     let mut serialize_fn_defs = vec![];
-    // let mut deserialize_fn_defs = vec![];
+    let mut deserialize_fn_defs = vec![];
 
-    for (i, (field, len)) in input.fields.iter_mut().filter_map(parse_big_array).enumerate() {
+    for (i, (field, len)) in input
+        .fields
+        .iter_mut()
+        .filter_map(parse_big_array)
+        .enumerate()
+    {
         let ty = &field.ty;
 
         if serialize {
@@ -82,10 +89,46 @@ pub fn serbia(_attr: TokenStream, item: TokenStream) -> TokenStream {
             });
         }
         if deserialize {
-            let fn_name = format_ident!("serbia_deserialize_{}_arr_{}", struct_name, i);
+            let fn_ident = format_ident!("serbia_deserialize_{}_arr_{}", struct_name, i);
+            let fn_name = fn_ident.to_string();
+            let count = 0..len;
 
             field.attrs.push(parse_quote! {
                 #[serde(deserialize_with = #fn_name)]
+            });
+
+            deserialize_fn_defs.push(quote! {
+                fn #fn_ident<'de, D>(deserializer: D) -> core::result::Result<#ty, D::Error>
+                where
+                    D: serde::Deserializer<'de>,
+                {
+                    struct ArrayVisitor;
+
+                    impl<'de> serde::de::Visitor<'de> for ArrayVisitor {
+                        type Value = #ty;
+
+                        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                            formatter.write_str(std::concat!("an array of length ", #len))
+                        }
+
+                        #[inline]
+                        fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+                        where
+                            A: serde::de::SeqAccess<'de>,
+                        {
+                            Ok([
+                                #(
+                                    match seq.next_element()? {
+                                        Some(val) => val,
+                                        None => return Err(serde::de::Error::invalid_length(#count, &self)),
+                                    }
+                                ),*
+                            ])
+                        }
+                    }
+
+                    deserializer.deserialize_tuple(#len, ArrayVisitor)
+                }
             });
         }
     }
@@ -93,6 +136,7 @@ pub fn serbia(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let expanded = quote! {
         #input
         #(#serialize_fn_defs)*
+        #(#deserialize_fn_defs)*
     };
 
     TokenStream::from(expanded)
