94051c6eaf6b9acb43b01ebde33761d884af1713
Tomasz Kurcz <uint@lavabit.com>
2021-03-10 09:41:24 +0100

    cargo fmt

--- tests/tests.rs
+++ tests/tests.rs
@@ -27,17 +27,9 @@ fn regular_struct_roundtrip() {
 fn tuple_struct_roundtrip() {
     #[serbia]
     #[derive(Debug, Serialize, Deserialize, PartialEq)]
-    struct S (
-        [u8; 300],
-        [u8; 42],
-        [u8; 8],
-    );
+    struct S([u8; 300], [u8; 42], [u8; 8]);
 
-    let original = S (
-        [0; 300],
-        [0; 42],
-        [0; 8],
-    );
+    let original = S([0; 300], [0; 42], [0; 8]);
 
     let serialized = serde_yaml::to_string(&original).unwrap();
     let deserialized = serde_yaml::from_str(&serialized).unwrap();
