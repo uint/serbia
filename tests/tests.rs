08c9d022a460cd026e1f45cd76070d80876f1c4c
Tomasz Kurcz <uint@lavabit.com>
2021-03-22 17:41:05 +0100

    Revert "Migrate integration tests to trybuild"
    This reverts commit bc423b6ca61bbf1cfb0e3ba220a634baa000382c.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,13 +1,99 @@
-// We use trybuild so that we can also test for what compiles and what doesn't.
-// Each test case is a file in the `trybuild` directory and essentially a minimal application.
-//
-// https://github.com/dtolnay/trybuild
+use serbia::serbia;
+use serde::{Deserialize, Serialize};
 
 #[test]
-fn trybuild() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/trybuild/01-regular-struct-roundtrip.rs");
-    t.pass("tests/trybuild/02-tuple-struct-roundtrip.rs");
-    t.pass("tests/trybuild/03-enum-roundtrip.rs");
-    t.pass("tests/trybuild/04-type-alias.rs");
+fn regular_struct_roundtrip() {
+    #[serbia]
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    struct S {
+        arr_a: [u8; 300],
+        arr_b: [u8; 42],
+        arr_small: [u8; 8],
+    }
+
+    let original = S {
+        arr_a: [0; 300],
+        arr_b: [0; 42],
+        arr_small: [0; 8],
+    };
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
+}
+
+#[test]
+fn tuple_struct_roundtrip() {
+    #[serbia]
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    struct S([u8; 300], [u8; 42], [u8; 8]);
+
+    let original = S([0; 300], [0; 42], [0; 8]);
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
+}
+
+#[test]
+fn enum_roundtrip() {
+    #[serbia]
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    enum E {
+        ArrBig([u8; 300]),
+        ArrSmall([u8; 22]),
+        Mixed([u8; 8], [i32; 44], String),
+    }
+
+    // 1
+    let original = E::ArrBig([22; 300]);
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
+
+    // 2
+    let original = E::ArrSmall([44; 22]);
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
+
+    // 3
+    let original = E::Mixed([0; 8], [5; 44], "asd".to_string());
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
+}
+
+#[test]
+fn type_alias() {
+    const BUFSIZE: usize = 300;
+    type BigArray = [i32; BUFSIZE];
+
+    #[serbia]
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    struct S {
+        #[serbia_bufsize(BUFSIZE)]
+        arr_a: BigArray,
+        arr_b: [u8; 42],
+        arr_small: [u8; 8],
+    }
+
+    let original = S {
+        arr_a: [0; BUFSIZE],
+        arr_b: [0; 42],
+        arr_small: [0; 8],
+    };
+
+    let serialized = serde_yaml::to_string(&original).unwrap();
+    let deserialized = serde_yaml::from_str(&serialized).unwrap();
+
+    assert_eq!(original, deserialized);
 }
